fe6ae9fc967054b3b2ccb3fcad375d18d0fff30e
Fanfnix <valentin@familledenis.net>
2026-03-24 14:56:31 +0100

    Map

---
+++ src/main/java/com/fnx/fpsconsole/Map.java
@@ -0,0 +1,18 @@
+package com.fnx.fpsconsole;
+
+public class Map {
+    private final int[][] maze;
+    private final int height;
+    private final int width;
+
+    public Map(final int height, final int width) {
+        this.height = height;
+        this.width = width;
+        this.maze = new int[height][width];
+        for (int row = 0; row < this.height; row++) {
+            for (int column = 0; column < this.width; column++) {
+                this.maze[row][column] = 0;
+            }
+        }
+    }
+}
